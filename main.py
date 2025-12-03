d3d1ea2b2ed2c090c06a5870e434747ee18b374b
Julia Bush <j.elizabethbush@gmail.com>
2025-12-03 21:06:34 +0100

    fix: updating import path and adding screen width and height text

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-from constants.py import SCREEN_WIDTH, SCREEN_HEIGHT
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 import pygame
 
 def main():
-    print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
+    print(f"Starting Asteroids with pygame version: {pygame.version.ver}, Screen width: {SCREEN_WIDTH} \n Screen height: {SCREEN_HEIGHT}")
 
 
 if __name__ == "__main__":
