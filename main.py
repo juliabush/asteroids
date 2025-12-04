b9b803c139824324331ae3f6a12643b75a338dd6
Julia Bush <j.elizabethbush@gmail.com>
2025-12-04 21:43:58 +0100

    update: changing method on asteroid after large on eis hit with shot to split instead of kill

--- main.py
+++ main.py
@@ -54,7 +54,7 @@ def main():
                 if asteroid.collides_with(shot):
                     log_event("asteroid_shot")
                     shot.kill()
-                    asteroid.kill()
+                    asteroid.split()
             if asteroid.collides_with(player_object):
                 log_event("player_hit")
                 print("Game Over!")
