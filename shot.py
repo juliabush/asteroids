c0de78341a419c0be96e887c7d8cd6b2709b4288
Julia Bush <j.elizabethbush@gmail.com>
2025-12-04 19:06:13 +0100

    add: imports necessary for inheritance

--- shot.py
+++ shot.py
@@ -1,5 +1,9 @@
+import pygame
+from circleshape import CircleShape
+from constants import LINE_WIDTH, WHITE
+
 class Shot(CircleShape):
-     def __init__(self, x, y, radius):
+    def __init__(self, x, y, radius):
         super().__init__(x, y, radius)
 
 
