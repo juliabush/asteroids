d515568516dadaada94f2ade1667cda6a9d8c4da
Julia Bush <j.elizabethbush@gmail.com>
2025-12-04 19:36:47 +0100

    fix: removing radius parameter and passing SHOT_RADIUS to super() init

--- shot.py
+++ shot.py
@@ -1,10 +1,10 @@
 import pygame
 from circleshape import CircleShape
-from constants import LINE_WIDTH, WHITE
+from constants import LINE_WIDTH, WHITE, SHOT_RADIUS
 
 class Shot(CircleShape):
-    def __init__(self, x, y, radius):
-        super().__init__(x, y, radius)
+    def __init__(self, x, y):
+        super().__init__(x, y, SHOT_RADIUS)
 
 
     def draw(self, screen):
