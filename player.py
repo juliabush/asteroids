39b4138f2feb8ceffa198d0eaa5ef8ad768279a8
Julia Bush <j.elizabethbush@gmail.com>
2025-12-04 15:11:03 +0100

    add: multiplying the vector by player speed and dt so the vector is the length the player should move

--- player.py
+++ player.py
@@ -33,3 +33,4 @@ class Player(CircleShape):
     def move(self, dt):
         unit_vector = pygame.Vector2(0,1)
         rotated_vector = unit_vector.rotate(self.rotation)
+        rotated_with_speed_vector = rotated_vector * PLAYER_SPEED * dt
