d55b4a8e7685f54923d63e52dfc8ecad42d0c06b
Julia Bush <j.elizabethbush@gmail.com>
2025-12-04 17:27:14 +0100

    add: overiding draw method, adding parameters, initializing asteroid params

--- asteroid.py
+++ asteroid.py
@@ -2,3 +2,10 @@ from constants import LINE_WIDTH
 
 class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
+        self.x = x
+        self.y = y
+        self.radius = radius
+
+    def draw(screen, "white", (0,0), radius, LINE_WIDTH):
+        pygame.draw.circle(screen, "white", (0,0), radius, LINE_WIDTH)
+    def update():
