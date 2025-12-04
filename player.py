ec5c5dd3e52254461656d340f35ac6abb212d1fa
Julia Bush <j.elizabethbush@gmail.com>
2025-12-04 13:49:25 +0100

    fixes: adding extra imports and fixing indentations

--- player.py
+++ player.py
@@ -1,11 +1,11 @@
-from constants import PLAYER_RADIUS
+import pygame
+from circleshape import CircleShape
+from constants import PLAYER_RADIUS, LINE_WIDTH
 
 class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
-
-
 
     # in the Player class
     def triangle(self):
@@ -14,4 +14,7 @@ class Player(CircleShape):
         a = self.position + forward * self.radius
         b = self.position - forward * self.radius - right
         c = self.position - forward * self.radius + right
-    return [a, b, c]
+        return [a, b, c]
+
+    def draw(self, screen):
+        pygame.draw.polygon(screen, "white", self.triangle(), LINE_WIDTH)
