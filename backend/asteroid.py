b6bb6694ce0bde99d0cab38bd89f79e8bfa9daf4
Julia Bush <j.elizabethbush@gmail.com>
2026-01-31 23:27:03 +0100

    update: remmoving commented out code

--- backend/asteroid.py
+++ backend/asteroid.py
@@ -15,21 +15,6 @@ class Asteroid(CircleShape):
         pygame.draw.circle(screen, WHITE, self.position, self.radius, LINE_WIDTH)
     def update(self, dt):
         self.position += self.velocity * dt
-    # def split(self):
-    #     self.kill()
-    #     if self.radius <= ASTEROID_MIN_RADIUS:
-    #         return
-    #     else:
-    #         log_event("asteroid_split")
-    #         angle = random.uniform(20, 50)
-    #         first_vector = self.velocity.rotate(+angle)
-    #         second_vector = self.velocity.rotate(-angle)
-    #         old_radius = self.radius
-    #         new_radius = old_radius - ASTEROID_MIN_RADIUS
-    #         first_asteroid = Asteroid(self.position.x, self.position.y, new_radius)
-    #         second_asteroid = Asteroid(self.position.x, self.position.y, new_radius)
-    #         first_asteroid.velocity = first_vector * 1.2
-    #         second_asteroid.velocity = second_vector * 1.2
 
     def split(self):
         self.kill()
